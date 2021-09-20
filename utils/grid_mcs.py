a25db7280109af5c022ebd671a596525b1dc26ef
leonardo <las3@okibrasil.cin.ufpe.br>
2021-09-19 21:25:24 -0300

    adds new shap results for METADES

--- utils/grid_mcs.py
+++ utils/grid_mcs.py
@@ -9,17 +9,23 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score, roc_auc_score
 
 
-def return_model_by_name(model_name, pool, k, ds_tech=0, IH_rate=0):
+
+dfp = [True, False]
+with_ih = [True, False]
+learning_rate_init: [0.0001, 0.001, 0.01]
+
+
+def return_model_by_name(model_name, pool, k, IH_rate=0):
     if model_name == "METADES":
-        return METADES(pool_classifiers=pool, k=k, random_state=888)
+        return METADES(pool_classifiers=pool, k=k, IH_rate=IH_rate,random_state=888)
     if model_name == "KNORAU":
-        return KNORAU(pool_classifiers=pool, k=k, random_state=888)
+        return KNORAU(pool_classifiers=pool, k=k, IH_rate=IH_rate, random_state=888)
     if model_name == "KNORAE":
-        return KNORAE(pool_classifiers=pool, k=k, random_state=888)
+        return KNORAE(pool_classifiers=pool, k=k, IH_rate=IH_rate, random_state=888)
     if model_name == "OLA":
-        return OLA(pool_classifiers=pool, k=k, random_state=888)
+        return OLA(pool_classifiers=pool, k=k, IH_rate=IH_rate, random_state=888)
     if model_name == "LCA":
-        return LCA(pool_classifiers=pool, k=k, random_state=888)
+        return LCA(pool_classifiers=pool, k=k, IH_rate=IH_rate, random_state=888)
     if model_name == "SingleBest":
         return SingleBest(pool_classifiers=pool, random_state=888)
     if model_name == "StaticSelection":
@@ -40,7 +46,7 @@ def cross_validation_model(X_train, y_train, X_test, y_test, model_name, pool, k
     return acc, roc
 
 
-def simple_gridSearch(model_name, pools, X_train, y_train, X_test, y_test):
+def simple_gridSearch(model_name, pool, X_train, y_train, X_test, y_test):
 
     best_acc = 0
     best_roc = 0
@@ -50,24 +56,26 @@ def simple_gridSearch(model_name, pools, X_train, y_train, X_test, y_test):
     list_k = [3, 7, 5, 9, 13]
     n_estimators_list = [10, 20, 30, 50, 70, 100]
 
-    if(model_name == "OLP"):
-        ds_tech_list = ['ola', 'lca', 'mcb', 'mla', 'a_priori', 'a_posteriori']
+    if(model_name == "METADES" or model_name == "KNORAU"or model_name == "KNORAE"):
         IH_rate_list = [0.0, 0.1, 0.2, 0.3, 0.5, 0.7, 1.0]
         best_ds_tech = ''
         best_ihrate = 0.0
         for k in list_k:
-            for ds_tech in ds_tech_list:
-                for IH_rate in IH_rate_list:
+            for IH_rate in IH_rate_list:
+                for n in n_estimators_list:
+                    pool_p = BaggingClassifier(
+                        base_estimator=pool, n_estimators=n)
+                    pool_p.fit(X_train, y_train)
                     acc, roc = cross_validation_model(
-                        X_train, y_train, X_test, y_test, model_name, None, k, ds_tech, IH_rate)
+                        X_train, y_train, X_test, y_test, model_name, pool_p, k, IH_rate)
                     if(roc >= best_roc):
                         if(acc > best_acc):
                             best_acc = acc
                             best_roc = roc
                             best_k = k
-                            best_ds_tech = ds_tech
                             best_ihrate = IH_rate
-        return return_model_by_name(model_name, None, best_k, best_ds_tech, best_ihrate)
+                            best_pool = pool_p
+        return return_model_by_name(model_name, best_pool, best_k, best_ihrate)
 
     if(model_name != "SingleBest" and model_name != "StaticSelection"):
 
